05adcf8ec1c9a0dc0b774fe0fbdad7cf9156350e
ddS08 <80191264+ddS08@users.noreply.github.com>
2023-05-24 11:06:37 +0530

    11:06am (24/05)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,6 +8,9 @@ function Todo(props){
     function deleteHandler(){
         setModalIsOpen(true);
     }
+    function closeModalHandler(){
+        setModalIsOpen(false);
+    }
 
     return(
     <div className="card">
@@ -15,9 +18,11 @@ function Todo(props){
             <div className="actions">
                 <button className="btn" onClick={deleteHandler}>Delete</button>
             </div>
-            {modalIsOpen && <Modal/>}
-            {modalIsOpen && <Backdrop/>}
-    
+            {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+            {modalIsOpen && <Backdrop  onCancel={closeModalHandler}/>}
+            {/*not an inbuilt component and hence cannot directly use onClick
+            hence we create a different function in backdrop and pass on the 
+            function to div whixh can have onClick directly*/}
     </div>);
 }
 export default Todo;
